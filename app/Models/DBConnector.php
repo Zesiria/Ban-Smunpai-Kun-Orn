a580c6049fd281e70ef5e457b455d80ab5bcab2d
Zesiria <78583534+Zesiria@users.noreply.github.com>
2022-10-30 15:23:38 +0700

    Implement Course controllers

--- app/Models/DBConnector.php
+++ app/Models/DBConnector.php
@@ -13,4 +13,22 @@ class DBConnector extends Model
     public function getCourse(){
         return DB::select('select * from COURSE');
     }
+
+    public function addCourse($course_name, $course_price){
+        return DB::insert('INSERT INTO COURSE VALUES
+                       (' . $course_name . ',' . $course_price . ')');
+    }
+
+    public function getCourseById($id){
+        $courses = DB::select('SELECT course_id FROM COURSE WHERE course_id = ' . $id);
+        $courses = json_decode(json_encode($courses), true);
+        return implode("",$courses[0]);
+    }
+
+    public function updateCourse($course_id, $course_name, $course_price){
+
+        DB::update("UPDATE COURSE SET course_name = '{$course_name}',
+        course_price = {$course_price} WHERE course_id = {$course_id}");
+        return "Successfully saved.";
+    }
 }
