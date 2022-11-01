efc808bb99e83221e2af1403c35de529bd05c35c
Zesiria <78583534+Zesiria@users.noreply.github.com>
2022-11-01 15:54:08 +0700

    Add resource controller

---
+++ app/Http/Controllers/Resource/ToolController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Resource;
+
+use App\Http\Controllers\Controller;
+use App\Models\DBConnector;
+use Illuminate\Http\Request;
+
+class ToolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $dbConnector = new DBConnector();
+        return $dbConnector->getCourse();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'tool_name' => ['min:2', 'max:30', 'string', 'required']
+        ]);
+
+        $tool_name = $request->course_name;
+
+        $dbConnector = new DBConnector();
+        $dbConnector->addTool($tool_name);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $dbConnector = new DBConnector();
+        $tools = $dbConnector->getCourse();
+
+        return $tools[$id];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'tool_id' => ['required'],
+            'tool_name' => ['min:2', 'max:30', 'string', 'required']
+        ]);
+
+        $dbConnector = new DBConnector();
+
+        try {
+            $tool_id = $dbConnector->getToolById($request->tool_id);
+        } catch (\Exception) {
+            throw new \Exception("This tool is not exist.");
+        }
+
+        $new_tool_name = $request->tool_name;
+
+        $dbConnector->updateTool($tool_id, $new_tool_name);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
