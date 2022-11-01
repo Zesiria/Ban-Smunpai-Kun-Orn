f78e4688f4a5fe645e419071f0821519fef27467
Zesiria <78583534+Zesiria@users.noreply.github.com>
2022-11-01 18:08:05 +0700

    Implement ServiceOrder - Time Resource Controller

--- app/Http/Controllers/Resource/ServiceOrderController.php
+++ app/Http/Controllers/Resource/ServiceOrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Resource;
 
 use App\Http\Controllers\Controller;
+use App\Models\DBConnector;
 use Illuminate\Http\Request;
 
 class ServiceOrderController extends Controller
@@ -14,7 +15,8 @@ class ServiceOrderController extends Controller
      */
     public function index()
     {
-        //
+        $dbConnector = new DBConnector();
+        return $dbConnector->getServiceOrder();
     }
 
     /**
@@ -35,7 +37,22 @@ class ServiceOrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'customer_id' => ['required','exists:customers'],
+            'employee_id' => ['required','exists:employees'],
+            'course_id' => ['required','exists:courses'],
+            'price' => ['min:0', 'max:9999', 'numberic', 'required'],
+            'date_time' => ['required','exists:times']
+        ]);
+
+        $customer_id = $request->customer_id;
+        $course_id = $request->course_id;
+        $employee_id = $request->employee_id;
+        $price = $request->price;
+        $date_time = $request->date_time;
+
+        $dbConnector = new DBConnector();
+        $dbConnector->addServiceOrder($customer_id,$course_id,$employee_id,$price,$date_time);
     }
 
     /**
@@ -46,7 +63,10 @@ class ServiceOrderController extends Controller
      */
     public function show($id)
     {
-        //
+        $dbConnector = new DBConnector();
+        $service_orders = $dbConnector->getServiceOrder();
+
+        return $service_orders[$id];
     }
 
     /**
@@ -69,7 +89,31 @@ class ServiceOrderController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'service_order_id' => ['required', 'exists:service_orders'],
+            'customer_id' => ['required','exists:customers'],
+            'employee_id' => ['required','exists:employees'],
+            'course_id' => ['required','exists:courses'],
+            'price' => ['min:0', 'max:9999', 'numberic', 'required'],
+            'date_time' => ['required','exists:times']
+        ]);
+
+        $service_order_id = $request->service_order_id;
+        $customer_id = $request->customer_id;
+        $course_id = $request->course_id;
+        $employee_id = $request->employee_id;
+        $price = $request->price;
+        $date_time = $request->date_time;
+
+        if($service_order_id != $id){
+            return response()->json([
+                "success" => false
+            ]);
+        }
+
+        $dbConnector = new DBConnector();
+        $dbConnector->updateServiceOrder($service_order_id, $customer_id, $course_id,$employee_id,$price,$date_time);
+
     }
 
     /**
