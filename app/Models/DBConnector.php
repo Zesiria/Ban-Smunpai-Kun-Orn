bde569902aa14183a48ccf3a535bb6fda4333a7e
Zesiria <78583534+Zesiria@users.noreply.github.com>
2022-11-01 14:40:59 +0700

    Update more function in DBConnector

--- app/Models/DBConnector.php
+++ app/Models/DBConnector.php
@@ -17,7 +17,7 @@ class DBConnector extends Model
 
     public function addCourse($course_name, $course_price){
         return DB::insert("INSERT INTO COURSE(course_name, course_price) VALUES(
-        {$course_name},{$course_price})");
+        '{$course_name}',{$course_price})");
     }
 
     public function getCourseById($id){
@@ -33,12 +33,12 @@ class DBConnector extends Model
     }
 
     public function getMaterial(){
-        $materials = DB::select("SELECT * FROM Material");
+        $materials = DB::select("SELECT * FROM MATERIAL");
         return json_decode(json_encode($materials), true);
     }
 
     public function addMaterial($material_name, $quantity, $minimum_value){
-        return DB::insert("INSERT INTO MATERIAL(material_name, quantit, minimum_value) VALUES({$material_name},{$quantity},{$minimum_value})");
+        return DB::insert("INSERT INTO MATERIAL(material_name, quantity, minimum_value) VALUES('{$material_name}',{$quantity},{$minimum_value})");
     }
 
     public function updateMaterial($material_id, $material_name, $quantity, $minimum_value){
@@ -57,11 +57,11 @@ class DBConnector extends Model
     }
 
     public function addTool($tool_name){
-        return DB::insert("INSERT INTO TOOL(tool_name) VALUES({$tool_name})");
+        return DB::insert("INSERT INTO TOOL(tool_name) VALUES('{$tool_name}')");
     }
 
     public function updateTool($tool_id, $tool_name){
-        return DB::update("UPDATE TOOL SET tool_name={$tool_name} WHERE tool_id = $tool_id");
+        return DB::update("UPDATE TOOL SET tool_name='{$tool_name}' WHERE tool_id = {$tool_id}");
     }
 
     public function getToolById($id){
@@ -70,6 +70,114 @@ class DBConnector extends Model
         return implode("",$tools[0]);
     }
 
-    
+    public function getCustomer(){
+        $customers = DB::select("SELECT * FROM CUSTOMER");
+        return json_decode(json_encode($customers),true);
+    }
 
+    public function addCustomer($customer_name, $email, $password, $phone_number){
+        return DB::insert("INSERT INTO CUSTOMER(customer_name, email, password, phone_number) VALUES('{$customer_name}','{$email}','{$password}','{$phone_number}')");
+    }
+
+    public function updateCustomer($customer_id, $customer_name, $email, $password, $phone_number){
+        return DB::update("UPDATE CUSTOMER SET customer_name='{$customer_name}',
+                email='{$email}', password='{$password}', phone_number='{$phone_number}'
+                WHERE customer_id = {$customer_id}");
+    }
+
+    public function getEmployee(){
+        $employees = DB::select("SELECT * FROM EMPLOYEE");
+        return json_decode(json_encode($employees), true);
+    }
+
+    public function addEmployee(){
+        //todo implement
+    }
+
+    public function updateEmployee(){
+        //todo implement
+    }
+
+    public function getTime(){
+        $times = DB::select("SELECT * FROM TIME");
+        return json_decode(json_encode($times),1);
+    }
+
+    public function addTime(){
+        //todo implement
+    }
+
+    public function updateTime(){
+        //todo implement
+    }
+
+    public function getServiceOrder(){
+        $serviceOrders = DB::select("SELECT * FROM SERVICE_ORDER");
+        return json_decode(json_encode($serviceOrders),1);
+    }
+
+    public function addServiceOrder(){
+        //todo implement
+    }
+
+    public function updateServiceOrder(){
+        //todo implement
+    }
+
+    // Pivot Table
+    public function getQueue(){
+        $queues = DB::select("SELECT * FROM QUEUE");
+        return json_decode(json_encode($queues),1);
+    }
+
+    public function addQueue(){
+        //todo implement
+    }
+
+    public function updateQueue(){
+        //todo implement
+    }
+
+    public function getEmployeeUsed(){
+        $employeeUses = DB::select("SELECT * FROM EMPLOYEE_USED");
+        return json_decode(json_encode($employeeUses),1);
+    }
+    public function addEmployeeUsed(){
+        //todo implement
+    }
+    public function updateEmployeeUsed(){
+        //todo implement
+    }
+
+    public function getEmployeeRequired(){
+        $employeeRequires = DB::select("SELECT * FROM EMPLOYEE_REQUIRED");
+        return json_decode(json_encode($employeeRequires),1);
+    }
+    public function addEmployeeRequired(){
+        //todo implement
+    }
+    public function updateEmployeeRequired(){
+        //todo implement
+    }
+
+    public function getCourseUsed(){
+        $courseUses = DB::select("SELECT * FROM COURSE_USED");
+        return json_decode(json_encode($courseUses),1);
+    }
+    public function addCourseUsed(){
+
+    }
+    public function updateCourseUsed(){
+        //todo implement
+    }
+    public function getCourseRequired(){
+        $courseRequires = DB::select("SELECT * FROM COURSE_REQUIRED");
+        return json_decode(json_encode($courseRequires),1);
+    }
+    public function addCourseRequired(){
+        //todo implement
+    }
+    public function updateCourseRequired(){
+        //todo implement
+    }
 }
