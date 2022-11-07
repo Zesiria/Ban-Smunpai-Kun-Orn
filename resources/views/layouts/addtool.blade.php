3560c722bfb60afb1837549b5bb7606a17b1a210
FEELONE <thinnarat.j@ku.th>
2022-11-07 22:16:17 +0700

    Update

---
+++ resources/views/layouts/addtool.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <div class="flex">
+        <h1>Tool Name :</h1>
+        <input type="text">
+    </div>
+    <div class="flex">
+        <h1>Type :</h1>
+        <input type="text">
+</div>
--- resources/views/menuadd.blade.php
+++ resources/views/menuadd.blade.php
@@ -2,12 +2,21 @@
 
 @section('content')
     <div>
-        <select name="cars" id="type">
-            <option value="null">เลือกประเภท</option>
-            <option value="employee">พนักงาน</option>
-            <option value="tool">อุปกรณ์</option>
-            <option value="material">วัตถุดิบ</option>
-            <option value="course">บริการ</option>
+        <select name="type" id="type">
+            <option $value="null">เลือกประเภท</option>
+            <option $value="employee">พนักงาน</option>
+            <option $value="tool">อุปกรณ์</option>
+            <option $value="material">วัตถุดิบ</option>
+            <option $value="course">บริการ</option>
           </select>
+        @if($value == "employee")
+            @include('layouts.addemployee')
+        @elseif($value == "tool")
+            @include('layouts.addtool')
+        @elseif($value == "material")
+            @include('layouts.addmaterial')
+        @else
+            @include('layouts.addcourse')
+        @endif
     </div>
 @endsection 
